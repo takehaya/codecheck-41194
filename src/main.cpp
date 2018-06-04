78ed873d482e68b8f278ef02a8874b3700e14bed
TakeruHayasaka <taketarou2@gmail.com>
2018-06-04 20:20:39 +0900

    add cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,6 @@ using namespace std;
 int main(int argc, char *argv[])
 {
   // start from 1 to ignore script name; argv[0] will be a name of processing file.
-  for (int i = 1; i < argc; i++) {
-    printf ("argv[%i]: %s\n", i, argv[i]);
-  }
+  printf ("Hello %s!", argv[i]);
   return 0;
 }
